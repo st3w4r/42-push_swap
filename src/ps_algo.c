ec9b5a6ec500371bc7f85d3c030cbaa67221ee01
Yanael Barbier <ybarbier@student.42.fr>
2015-03-24 19:36:29 +0100

    Function ra ou rra

--- src/ps_algo.c
+++ src/ps_algo.c
@@ -39,28 +39,57 @@ static int		ps_max_stack(t_stack *stack)
 	}
 	return (max);
 }
-/*
-void	ps_ra_rra(t_stack *stack)
-{
 
+int		ps_ra_rra(t_stack *stack)
+{
+	int size_mid_stack;
+	int count_rotate;
+
+	size_mid_stack = (ps_stack_size(stack) / 2);
+
+	count_rotate = 0;
+	while (stack->down)
+	{
+		if (stack->down->nbr > stack->nbr)
+			++count_rotate;
+		stack = stack->down;
+	}
+
+	// ft_putnbr(count_rotate);
+	if (count_rotate < size_mid_stack)
+		return (-1); //RRA
+	else
+		return (1); //RA
 }
-*/
+
 void	ps_algo_sort(t_env *env)
 {
 	// int max;
 	int min;
+	int state;
 
 	if (ps_stack_empty(&(env->stack_a)) == 0)
 		return ;
 	min = ps_min_stack(env->stack_a);
 	while (ps_stack_is_sorted(env->stack_a) == 0)
 	{
-		if (env->stack_a->down)
+		if (!env->stack_a->down)
+			return ;
+
+		if (env->stack_a->down->nbr != min &&
+			env->stack_a->down->nbr < env->stack_a->nbr)
 		{
-			if (env->stack_a->down->nbr != min &&
-				env->stack_a->down->nbr < env->stack_a->nbr)
-				ps_operator_sa(env,1);
-			ps_operator_ra(env, 1);
+			ps_operator_sa(env,1);
+			state = ps_ra_rra(env->stack_a);
 		}
+
+		if (state)
+		{
+			ps_ra_rra(env->stack_a);
+			ps_ra_rra(env->stack_a);
+		}
+
+		ps_operator_ra(env, 1);
+		ps_operator_rra(env, 1);
 	}
 }
